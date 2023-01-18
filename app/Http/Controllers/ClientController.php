7d8ea277810ac1fcb4d224fd328c3588f4ce6c2a
Cauã Arseno <cauaarseno61@gmail.com>
2023-01-17 23:28:25 -0300

    rotas no response

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -22,6 +22,6 @@ class ClientController extends Controller
     public function show()
     {
         
-        return response()->json($client->client);
+        return response('/clients/show/{client}')->json($client);
     }
 }
